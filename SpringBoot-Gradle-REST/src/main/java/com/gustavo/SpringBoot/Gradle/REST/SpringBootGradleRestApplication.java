e91824360cfae53afcc1f20254ef86184d8f4e5b
Gustavo Cigaran <gustavocigaran@gmail.com>
2020-04-19 18:49:04 -0300

    Demonstração de um projeto criado com Springboot + Gradle + Rest Repositories

---
+++ SpringBoot-Gradle-REST/src/main/java/com/gustavo/SpringBoot/Gradle/REST/SpringBootGradleRestApplication.java
@@ -0,0 +1,15 @@
+package com.gustavo.SpringBoot.Gradle.REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleRestApplication.class, args);
+
+		System.out.println("EAE RAPAZIADA");
+	}
+
+}
