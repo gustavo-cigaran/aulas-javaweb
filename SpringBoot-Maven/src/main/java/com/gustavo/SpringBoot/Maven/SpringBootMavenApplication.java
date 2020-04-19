e67ca47d47215f0c4551f4ca7f1b51db9d3ac3f0
Gustavo Cigaran <gustavocigaran@gmail.com>
2020-04-19 18:50:45 -0300

    Demonstração de um projeto criado com Springboot + Maven

---
+++ SpringBoot-Maven/src/main/java/com/gustavo/SpringBoot/Maven/SpringBootMavenApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.SpringBoot.Maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Maven/src/test/java/com/gustavo/SpringBoot/Maven/SpringBootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.SpringBoot.Maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
